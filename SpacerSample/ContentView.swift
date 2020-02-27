36cbef3ba951f6ac878eaa4d05a845e396a5809d
SamplesMaker <insert@kakao.com>
2020-02-28 08:47:52 +0900

    SpacerSample

--- SpacerSample/ContentView.swift
+++ SpacerSample/ContentView.swift
@@ -10,7 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        // Stack 상에서 남은 공각은 Spacer 를 넣어줌으로써 스택상에서 정렬(?)
+        VStack {
+            HStack {
+                Image(systemName: "1.circle.fill")
+                Image(systemName: "arrow.left.circle.fill")
+                Spacer()
+            }
+            
+            Spacer()
+            
+            HStack {
+                Image(systemName: "arrow.left.circle.fill")
+                Spacer()
+                Image(systemName: "2.circle.fill")
+                Spacer()
+                Image(systemName: "arrow.right.circle.fill")
+            }.padding()
+            
+            Spacer()
+            
+            HStack {
+                 Spacer()
+                 Image(systemName: "arrow.right.circle.fill")
+                 Image(systemName: "3.circle.fill")
+             }.padding()
+            
+        }.foregroundColor(Color.white)
+        .background(RoundedRectangle(cornerRadius: 10))
+        .foregroundColor(Color.blue)
+        .font(.largeTitle)
+        .padding()
+        .frame(width: 400, height: 400)
+    
     }
 }
 
